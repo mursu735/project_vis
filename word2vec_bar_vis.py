Fix mistake in coordinate calculation, continue with first case

word2vec_bar_vis.py
@@ -57,7 +57,8 @@ with open("filtered_coords.txt") as file:
         y = min(y, sizey - 1)
         if time not in coords_map:
             coords_map[time] = {"Symptom 1": np.zeros(13), "Symptom 2": np.zeros(13), "Other symptoms": np.zeros(13)}
-        pixel = pix[x, y]
+        y_tl = word2vec_helpers.get_height() - y - 1
+        pixel = pix[x, y_tl]
         asd = colors - pixel
         index = np.argmin(np.absolute(asd.sum(axis=1)))
         if any(symptom in text for symptom in symptom1):

word2vec_filter_for_first_case.py
@@ -1,5 +1,6 @@
 import logging
 import word2vec_helpers
+import numpy as np
 import gensim.downloader as api
 from gensim.test.utils import datapath
 from gensim import utils
@@ -26,70 +27,68 @@ def check_if_separator_is_used(separator):
         print("Row did not contain separator")
 
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
-
-model_name = word2vec_helpers.fetch_model_name()
-
-print(model_name)
-
-new_model = gensim.models.Word2Vec.load(f"gensim-model-{model_name}")
-
-#wv = api.load('word2vec-google-news-300')
-
-wv = new_model.wv
-
-
-print(wv.most_similar(positive=['sick'], topn=100))
-
-word_list = word2vec_helpers.get_word_list()
-
-word_distances = {}
-
-for i in range(0, len(word_list)):
-    for j in range(i+1, len(word_list)):
-        word = f"{word_list[i]} -> {word_list[j]}"
-        diff = wv.similarity(word_list[i], word_list[j])
-        word_distances[word] = diff
-
-print(word_distances)
-        
-
-
-separator = ':^:'
-edited_image_filename = "MC_1_Materials_3-30-2011/Vastopolis_Map_edited.png"
-im = Image.open(edited_image_filename) # Can be many different formats.
-pix = im.load()
-
+# If the position and date is in the area of interest, save it, otherwise skip it
+target_area = np.array([63, 72, 204])
 start_time = datetime(2011, 5, 17, 0, 0)
 end_time = datetime(2011, 5, 18, 0, 0)
 
+separator = ':^:'
+edited_image_filename = "MC_1_Materials_3-30-2011/Vastopolis_Map_edited_first_case_mod.png"
+im = Image.open(edited_image_filename) # Can be many different formats.
+pix = im.load()
+
+messages = {}
+total = 0
+
+times = []
+
+lines = []
+
 with open('MC_1_Materials_3-30-2011/Microblogs.csv') as csvfile:
     reader = pd.read_csv(csvfile, sep=",", header=0, usecols=["Created_at", "text", "Location"])
-    converted = reader.text.to_list()
-    coords = []
-    lines = []
     print(reader)
     for index, row in reader.iterrows():
-        split = row["Created_at"].split(" ")
-        time = split[0]
-        if any(substring in row["text"] for substring in word_list):
-            text = row["Location"] + separator + row["text"]
-            coords.append(text)
-            lines.append(row["Created_at"])
+        try:
+            time = datetime.strptime(row["Created_at"], '%m/%d/%Y %H:%M').replace(minute=0)
+            if time >= start_time and time <= end_time:
+                if time not in messages:
+                    messages[time] = []
+                    times.append(time)
+                pos = row["Location"]
+                x, y = word2vec_helpers.get_coords_in_pixels(pos)
+                # PIL has origin in top-left, convert bottom-left origin to this
+                y_tl = word2vec_helpers.get_height() - y - 1
+                color = np.array(pix[x, y_tl])
+                asd = target_area - color
+                sum = asd.sum()
+                if sum == 0:
+                    messages[time].append(row["text"])
+                    total += 1
+                    text = row["Created_at"] + separator + row["Location"] + separator + row["text"]
+                    lines.append(text)
+        except ValueError:
+            text = row["text"]
+            print(f"Invalid date: {time}, message: {text}")
+
+sorted_times = sorted(times)
 
 
-with open("filtered2.txt", "w") as file:
+with open("filtered_first_case.txt", "w") as file:
     for line in lines:
         file.write(f"{line}\n")
-'''
-        for line in val:
-            file.write(f"{line}\n")
-'''
 
+
+with open("filtered_first_case.txt", "w") as file:
+    for time in sorted_times:
+        file.write(f"{time}:\n")
+        for message in messages[time]:
+            file.write(f"{message}\n")
+
+
+'''
 with open("filtered_coords.txt", "w") as file:
     for line in coords:
         file.write(f"{line}\n")
-'''
         for line in val:
             file.write(f"{line}\n")
 '''