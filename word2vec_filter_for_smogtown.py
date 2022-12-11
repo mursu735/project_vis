25a318be74a7495fe3ba4cac954c3476788860f9
Sami Mairue <sami.mairue@aalto.fi>
2022-12-11 23:38:38 +0900

    Add filter for users in Smogtown

--- word2vec_filter_for_smogtown.py
+++ word2vec_filter_for_smogtown.py
@@ -42,14 +42,36 @@ def nltk_tag_to_wordnet_tag(nltk_tag):
     else:
         return None
 
+def select_rows(row, pix, sizex, sizey):
+    #print(row)
+    try:
+        time = datetime.strptime(row.Created_at, '%m/%d/%Y %H:%M').replace(minute=0)
+        if time == target_time:
+            pos = row.Location
+            x, y = word2vec_helpers.get_coords_in_pixels(pos)
+            x = min(x, sizex - 1)
+            y = min(y, sizey - 1)
+            # PIL has origin in top-left, convert bottom-left origin to this, then fetch pixel color
+            y_tl = word2vec_helpers.get_height() - y - 1
+            color = np.array(pix[x, y_tl])
+            asd = target_area - color[:3] # Some pixels return [r, g, b, alpha], get rid of alpha
+            sum = asd.sum()
+            if sum == 0:
+                return True
+    except ValueError as e:
+        text = row["text"]
+        print(f"{e}, message: {text}")
+        return False
+    return False
+
+
 
 # If the position and date is in the area of interest, save it, otherwise skip it
 target_area = np.array([63, 72, 204])
-start_time = datetime(2011, 5, 16, 0, 0)
-end_time = datetime(2011, 5, 19, 0, 0)
+target_time = datetime(2011, 5, 17, 9, 0)
 
 separator = ':^:'
-edited_image_filename = "MC_1_Materials_3-30-2011/Vastopolis_Map_edited_first_case_mod.png"
+edited_image_filename = "MC_1_Materials_3-30-2011/Vastopolis_Map_smogtown.png"
 im = Image.open(edited_image_filename) # Can be many different formats.
 pix = im.load()
 sizex, sizey = im.size
@@ -65,11 +87,31 @@ for element in tmp:
         other_symptoms.append(element)
 
 print(other_symptoms)
+messages = {}
 total = 0
 
 times = []
 
 lines = []
+
+ids = []
+
+
+# Get IDs that were in Smogtown when the explosion happened
+reader = pd.read_csv('MC_1_Materials_3-30-2011/Microblogs.csv', sep=",", header=0, usecols=["ID", "Created_at", "text", "Location"])
+print(reader)
+total = len(reader.index)
+count = 0
+reader['smogtown'] = reader.apply(select_rows, args=(pix, sizex, sizey), axis=1)
+smogtown = reader[reader['smogtown']]
+ids = smogtown["ID"].unique()
+print(ids)
+
+# Get messages only from those IDs
+
+reader = reader[reader["ID"].isin(ids)]
+
+#print(reader)
 
 nltk.download('wordnet')
 nltk.download('omw-1.4')
@@ -78,19 +120,44 @@ nltk.download('averaged_perceptron_tagger')
 
 lemmatizer = WordNetLemmatizer()
 
-#with open('MC_1_Materials_3-30-2011/Microblogs.csv') as csvfile:
-reader = pd.read_csv('MC_1_Materials_3-30-2011/Microblogs.csv', sep=",", header=0, usecols=["ID", "Created_at", "text", "Location"])
-print(reader)
-total = len(reader.index)
-count = 0
+lines = []
+
+for index, row in reader.iterrows():
+    time = datetime.strptime(row.Created_at, '%m/%d/%Y %H:%M').replace(minute=0)
+    prefix = ""
+    nltk_tagged = nltk.pos_tag(nltk.word_tokenize(row["text"]))
+    #tuple of (token, wordnet_tag)
+    wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
+    lemmatized_sentence = []
+    for word, tag in wordnet_tagged:
+        if tag is None:
+            #if there is no available tag, append the token as is
+            lemmatized_sentence.append(word)
+        else:
+            #else use the tag to lemmatize the token
+            lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
+    # "5/18/2011" in row["Created_at"] and
+    concat = ' '.join(lemmatized_sentence)
+    if any(substring in concat for substring in blacklist):
+        prefix = "None"
+    else:    
+        if any(substring in concat for substring in symptom1):
+            prefix = "Symptom 1"
+        elif any(substring in concat for substring in symptom2):
+            prefix = "Symptom 2"
+        elif any(substring in concat for substring in other_symptoms):
+            prefix = "Other"
+        else:
+            prefix = "None"
+    text = prefix + separator + row["Created_at"] + separator + row["Location"] + separator + row["text"] + separator + str(row["ID"])
+    lines.append(text)
+'''
 for index, row in reader.iterrows():
     try:
         print(f"{count}/{total}", end='\r')
         count += 1
         time = datetime.strptime(row["Created_at"], '%m/%d/%Y %H:%M').replace(minute=0)
-        if time >= start_time and time <= end_time:
-            if time not in times:
-                times.append(time)
+        if time == target_time:
             pos = row["Location"]
             x, y = word2vec_helpers.get_coords_in_pixels(pos)
             x = min(x, sizex - 1)
@@ -100,44 +167,20 @@ for index, row in reader.iterrows():
             color = np.array(pix[x, y_tl])
             asd = target_area - color[:3] # Some pixels return [r, g, b, alpha], get rid of alpha
             sum = asd.sum()
-            # if sum == 0:
-            prefix = ""
-            nltk_tagged = nltk.pos_tag(nltk.word_tokenize(row["text"]))
-            #tuple of (token, wordnet_tag)
-            wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
-            lemmatized_sentence = []
-            for word, tag in wordnet_tagged:
-                if tag is None:
-                    #if there is no available tag, append the token as is
-                    lemmatized_sentence.append(word)
-                else:
-                    #else use the tag to lemmatize the token
-                    lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
-            # "5/18/2011" in row["Created_at"] and
-            concat = ' '.join(lemmatized_sentence)
-            if any(substring in concat for substring in blacklist):
-                prefix = "None"
-            else:    
-                if any(substring in concat for substring in symptom1):
-                    prefix = "Symptom 1"
-                elif any(substring in concat for substring in symptom2):
-                    prefix = "Symptom 2"
-                elif any(substring in concat for substring in other_symptoms):
-                    prefix = "Other"
-                else:
-                    prefix = "None"
-            #if sum == 0:
-            #total += 1
-            text = prefix + separator + row["Created_at"] + separator + row["Location"] + separator + row["text"] + separator + str(row["ID"])
-            lines.append(text)
+            if sum == 0:
+                ids.append(row["ID"])
     except ValueError as e:
         text = row["text"]
         print(f"{e}, message: {text}")
+'''
+# Get messages only from those IDs
 
-sorted_times = sorted(times)
+#df = reader[reader["ID"].isin(ids)]
+
+#print(df)
 
 
-with open("filtered_first_case.txt", "w", encoding="utf-8") as file:
+with open("filtered_smogtown_users.txt", "w", encoding="utf-8") as file:
     for line in lines:
         file.write(f"{line}\n")
 
