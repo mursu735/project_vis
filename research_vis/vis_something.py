4f6223fa06c46ad16bf56e04a77059ee3a726461
Sami Mairue <sami.mairue@aalto.fi>
2023-02-06 15:53:46 +0900

    Add ability to select two words and get the most similar embeddings to them

--- research_vis/vis_something.py
+++ research_vis/vis_something.py
@@ -12,8 +12,22 @@ prev_word = ""
 model_name = helpers.fetch_model_name()
 new_model = gensim.models.Word2Vec.load(f"{model_name}").wv
 
-#for word in new_model.index_to_key:
-#    print(word)
+def get_most_similar_words(word1, word2):
+    words = []
+    similar = ""
+    similar2 = ""
+    similar_both = ""
+    if word1 != "":
+        similar = new_model.most_similar(positive=[f"{word1}"], topn=20)
+        words.append(word1)
+    if word2 != "":
+        similar2 = new_model.most_similar(positive=[f"{word2}"], topn=20)
+        words.append(word2)
+    if len(words) == 2:
+        similar_both = new_model.most_similar(positive=words, topn=20)
+    return similar, similar2, similar_both
+
+#tokens = sorted(new_model.index_to_key)
 
 def run_server():
     #wordcloud_fig = get_base_wordcloud_fig()
@@ -27,11 +41,12 @@ def run_server():
 
         html.Div([
         "Input: ",
-        dcc.Input(id='text-input', value='', type='text'),
+        dcc.Input(id='word-1', value='', type='text', style={"margin-left": "15px"}),
+        dcc.Input(id='word-2', value='', type='text', style={"margin-left": "15px"}),
         html.Button('Recalculate', n_clicks=0, id='recalculate'),
         ]),
 
-        html.Div(children=["Selected word", html.Pre(id='selected-word')]),
+        html.Div(children=["Selected words", html.Pre(id='selected-word')]),
 
 
         dbc.Row(
@@ -39,37 +54,51 @@ def run_server():
                 dbc.Col(html.Div([dcc.Markdown("""
                         **Click Data**
 
-                        Closest words in word2vec.
+                        Closest words to first word in word2vec.
                     """),
                     html.Pre(id='word2vec-data')])),
                     
                 dbc.Col(html.Div([dcc.Markdown("""
                     **Click Data**
 
-                    Words in the same cluster. 
+                    Closest words to second word in word2vec.
                 """),
-                html.Pre(id='cluster-data')]))
+                html.Pre(id='word2vec-data2')])),
+
+                dbc.Col(html.Div([dcc.Markdown("""
+                    **Click Data**
+
+                    Closest words to both words in word2vec.
+                """),
+                html.Pre(id='word2vec-data3')]))
             ]
         ),
+
+        #html.Div(children=[dcc.Markdown("***All available words***"), dcc.Markdown("\n".join(tokens))]),
     ])
     
     @app.callback(
         Output('selected-word', 'children'),
         Output('word2vec-data', 'children'),
-        Input('text-input', 'value'),
+        Output('word2vec-data2', 'children'),
+        Output('word2vec-data3', 'children'),
+        Input('word-1', 'value'),
+        Input('word-2', 'value'),
         Input('recalculate', 'n_clicks'))
-    def display_click_data(text, n_clicks):
+    def display_click_data(word1, word2, n_clicks):
         global prev_word
         ctx = dash.callback_context
         clicked_element = ctx.triggered[0]["prop_id"].split(".")[0]
         if clicked_element == "recalculate":
-            similar = new_model.most_similar(positive=[f"{text}"], topn=20)
+            similar, similar2, similar_both = get_most_similar_words(word1, word2)
             similar = "\n".join(map(str, similar))
-            print(similar)
-            prev_word = text
-            return text, similar
+            similar2 = "\n".join(map(str, similar2))
+            similar_both = "\n".join(map(str, similar_both))
+            #print(similar)
+            prev_word = " ".join([word1, word2])
+            return prev_word, similar, similar2, similar_both
         else:
-            return prev_word, ''
+            return prev_word, '', '', ''
         #return json.dumps(clickData, indent=2)
 
     if __name__ == '__main__':
